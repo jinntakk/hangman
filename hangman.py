02ddeaa1bcc8f7e0d8f8fff4fdafb946a79d1c74
jinntakk <takjinwoo@gmail.com>
2024-04-04 01:04:39 -0400

    hangman

---
+++ hangman.py
@@ -0,0 +1,43 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    lives = 6
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() #keeping track of used letters
+
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives, "lives left and you have used these letters:", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        user_letter = input("Type a letter: ").upper()
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter not in the word.")
+        elif user_letter in used_letters:
+            print("You have already used that letter")
+        else:
+            print("Can't do that")
+    if lives == 0:
+        print(f"You lose the word was {word}.")
+    else:
+        print(f"You guessed the word! {word}!")
+
+hangman()
